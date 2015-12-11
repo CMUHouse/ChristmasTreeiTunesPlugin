df136d5a08478856f1f0c866b0ac141d7495b0c0
Dmitriy Solomonov <solomonov@apple.com>
2015-12-11 00:12:16 -0800

    Updated for fancier lights

--- iTunesPlugInMac.mm
+++ iTunesPlugInMac.mm
@@ -65,6 +65,8 @@
 #include <deque>
 #include <bitset>
 
+#import <CoreImage/CoreImage.h>
+
 using namespace std;
 
 //-------------------------------------------------------------------------------------------------
@@ -104,6 +106,7 @@ extern "C" OSStatus iTunesPluginMainMachO( OSType inMessage, PluginMessageInfo *
 #endif	// USE_SUBVIEW
 
 static const size_t kNumTreeBits = 4;
+static const size_t kNumTreeProgrammableLights = 75;
 static const size_t kLPFSize = 10;
 
 
@@ -111,6 +114,9 @@ typedef std::vector<UInt8> SpectrumData;
 typedef std::array<float, kNumTreeBits> OutputLevels;
 typedef std::bitset<kNumTreeBits> TreeDisplayBits;
 typedef std::deque<OutputLevels> OutputLevelsQueue;
+
+static const bool kEmitSimpleBits = false;
+static const bool kEmitLEDRibbonIntensity = true;
 
 SpectrumData scaleSpectrumData(const SpectrumData& srcData,
                                size_t srcStart, size_t srcLength,
@@ -133,15 +139,14 @@ SpectrumData scaleSpectrumData(const SpectrumData& srcData,
         CGImageRef img = CGBitmapContextCreateImage(spectrumCtx);
 
         CGContextSetInterpolationQuality(dstCtx, kCGInterpolationHigh);
-        CGContextDrawImage(dstCtx, CGRectMake(0, 0, 4, 1), img);
-
+        CGContextDrawImage(dstCtx, CGRectMake(0, 0, dstLength, 1), img);
+        
         UInt8* dstCtxBytes = (UInt8*)CGBitmapContextGetData(dstCtx);
         outData.assign(dstCtxBytes, dstCtxBytes + dstLength);
         
         if (img) {
             CFRelease(img);
         }
-
     }
     
     if (spectrumCtx) {
@@ -187,9 +192,6 @@ OutputLevels outputLevelsQueueAverage(const OutputLevelsQueue& queue) {
 void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort )
 {
 
-    long fivePercentMax=0;
-	long fivePercentMin=0;
-    
     vector<UInt8> leftSpectrumData, rightSpectrumData;
     leftSpectrumData.reserve(kVisualNumSpectrumEntries);
     rightSpectrumData.reserve(kVisualNumSpectrumEntries);
@@ -213,20 +215,29 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
     
     const uint32_t spectSum = accumulate(spectrumData.begin(), spectrumData.end(), 0);
 
-    for(int i=kVisualNumSpectrumEntries-1;i>=0;i--){
-        fivePercentMax += spectrumData[i];
-		if(fivePercentMax >= spectSum*0.05){
-			fivePercentMax = i;
-			break;
-		}
-	}
-	for(int i=0;i<kVisualNumSpectrumEntries;i++){
-        fivePercentMin += spectrumData[i];
-		if(fivePercentMin >= spectSum*.05){
-			fivePercentMin = i;
-			break;
-		}
-	}
+    size_t fivePercentMax=0;
+    {
+        uint32_t sumLimit = 0;
+        for(int i=spectrumData.size()-1;i>=0;i--){
+            sumLimit += spectrumData[i];
+            if(sumLimit >= spectSum*0.05){
+                fivePercentMax = i;
+                break;
+            }
+        }
+    }
+	
+    size_t fivePercentMin=0;
+    {
+        uint32_t sumLimit = 0;
+        for(int i=0;i<spectrumData.size();i++){
+            sumLimit += spectrumData[i];
+            if(sumLimit >= spectSum*.05){
+                fivePercentMin = i;
+                break;
+            }
+        }
+    }
     
     if (fivePercentMax <= fivePercentMin) {
         fivePercentMax = fivePercentMin;
@@ -235,25 +246,33 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
     uint32_t specStart = fivePercentMin;
     uint32_t specWidth = fivePercentMax - fivePercentMin;
 
-    SpectrumData simpleData = scaleSpectrumData(spectrumData, specStart, specWidth, 4);
+    SpectrumData simpleData = scaleSpectrumData(spectrumData, specStart, specWidth, kNumTreeBits);
     
-    {
+    SpectrumData smallRibbon = scaleSpectrumData(spectrumData, specStart, specWidth, kNumTreeProgrammableLights/3);
+    SpectrumData ribbonData = scaleSpectrumData(smallRibbon, 0, smallRibbon.size(), kNumTreeProgrammableLights);
+    
+    NSSize viewSize = [visualPluginData->destView bounds].size;
+    CGFloat widthStep = viewSize.width / (CGFloat)spectrumData.size();
+    CGFloat heightStep = viewSize.height/512;
+    CGFloat widthBase = 10;
+    CGFloat heightBase = viewSize.height/4;
+    
+    if (spectrumData.size()) {
         CGRect drawRect = [visualPluginData->destView bounds];
         
         // fill the whole view with black to start
         [[NSColor blackColor] set];
         NSRectFill( drawRect );
-
-        
-        NSSize viewSize = [visualPluginData->destView bounds].size;
         
         NSBezierPath* thePath = [NSBezierPath bezierPath];
-        NSPoint pt = NSMakePoint(10, viewSize.height/4 + spectrumData[0]);
-        
+
+        NSPoint pt = NSMakePoint(widthBase,
+                                 heightBase + (spectrumData[0] * heightStep));
         [thePath moveToPoint:pt];
+        
         for(int i=1;i<fivePercentMin;i++){
-            pt = NSMakePoint(10+i*viewSize.width/kVisualNumSpectrumEntries,
-                             viewSize.height/4+(spectrumData[i])*viewSize.height/512);
+            pt = NSMakePoint(widthBase + (i*widthStep),
+                             heightBase + (spectrumData[i] * heightStep));
             [thePath lineToPoint:pt];
         }
         [thePath setLineWidth:3];
@@ -261,10 +280,12 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
         [thePath stroke];
         
         thePath = [NSBezierPath bezierPath];
-        pt = NSMakePoint(10+fivePercentMin*viewSize.width/kVisualNumSpectrumEntries, viewSize.height/4 + spectrumData[fivePercentMin] );
+        pt = NSMakePoint(widthBase + (fivePercentMin*widthStep),
+                         heightBase + (spectrumData[fivePercentMin] * heightStep));
         [thePath moveToPoint:pt];
         for(int i=fivePercentMin;i<fivePercentMax;i++){
-            pt = NSMakePoint(10+i*viewSize.width/kVisualNumSpectrumEntries,viewSize.height/4+spectrumData[i]*viewSize.height/512);
+            pt = NSMakePoint(widthBase + i*widthStep,
+                             heightBase + (spectrumData[i] * heightStep));
             [thePath lineToPoint:pt];
         }
         [thePath setLineWidth:3];
@@ -272,10 +293,13 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
         [thePath stroke];
 
         thePath = [NSBezierPath bezierPath];
-        pt = NSMakePoint(10+fivePercentMax*viewSize.width/kVisualNumSpectrumEntries, viewSize.height/4 + spectrumData[fivePercentMax]);
+        pt = NSMakePoint(widthBase + fivePercentMax*widthStep,
+                         heightBase + (spectrumData[fivePercentMax] * heightStep));
+
         [thePath moveToPoint:pt];
         for(int i=fivePercentMax;i<kVisualNumSpectrumEntries;i++){
-            pt = NSMakePoint(10+i*viewSize.width/kVisualNumSpectrumEntries,viewSize.height/4+spectrumData[i]*viewSize.height/512);
+            pt = NSMakePoint(widthBase + i*widthStep,
+                             heightBase + (spectrumData[i] * heightStep));
             [thePath lineToPoint:pt];
         }
         [thePath setLineWidth:3];
@@ -288,37 +312,39 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
         [[NSColor whiteColor] set];
         [thePath stroke];
 	
-        // should we draw the info/artwork in the bottom-left corner?
-        time_t		theTime = time( NULL );
+    }
+
+    static BOOL bPrevDidDrawArtwork = NO;
+    BOOL bDrawArtwork = NO;
+    
+    if ( time( NULL ) < visualPluginData->drawInfoTimeOut )
+    {
+        CGPoint where = CGPointMake( 10, 10 );
         
-        if ( theTime < visualPluginData->drawInfoTimeOut )
+        // if we have a song title, draw it (prefer the stream title over the regular name if we have it)
+        NSString *				theString = NULL;
+        
+        if ( visualPluginData->streamInfo.streamTitle[0] != 0 )
+            theString = [NSString stringWithCharacters:&visualPluginData->streamInfo.streamTitle[1] length:visualPluginData->streamInfo.streamTitle[0]];
+        else if ( visualPluginData->trackInfo.name[0] != 0 )
+            theString = [NSString stringWithCharacters:&visualPluginData->trackInfo.name[1] length:visualPluginData->trackInfo.name[0]];
+        
+        if ( theString != NULL )
         {
-            CGPoint where = CGPointMake( 10, 10 );
+            NSDictionary *		attrs = [NSDictionary dictionaryWithObjectsAndKeys:[NSColor whiteColor], NSForegroundColorAttributeName, NULL];
             
-            // if we have a song title, draw it (prefer the stream title over the regular name if we have it)
-            NSString *				theString = NULL;
-            
-            if ( visualPluginData->streamInfo.streamTitle[0] != 0 )
-                theString = [NSString stringWithCharacters:&visualPluginData->streamInfo.streamTitle[1] length:visualPluginData->streamInfo.streamTitle[0]];
-            else if ( visualPluginData->trackInfo.name[0] != 0 )
-                theString = [NSString stringWithCharacters:&visualPluginData->trackInfo.name[1] length:visualPluginData->trackInfo.name[0]];
-            
-            if ( theString != NULL )
-            {
-                NSDictionary *		attrs = [NSDictionary dictionaryWithObjectsAndKeys:[NSColor whiteColor], NSForegroundColorAttributeName, NULL];
-                
-                [theString drawAtPoint:where withAttributes:attrs];
-            }
-            
-            // draw the artwork
-            if ( visualPluginData->currentArtwork != NULL )
-            {
-                where.y += 40;
-                
-                [visualPluginData->currentArtwork drawAtPoint:where fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.75];
-            }
+            [theString drawAtPoint:where withAttributes:attrs];
         }
         
+        // draw the artwork
+        if ( visualPluginData->currentArtwork != NULL )
+        {
+            where.y += 40;
+            
+            [visualPluginData->currentArtwork drawAtPoint:where fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.75];
+        }
+        
+        bDrawArtwork = YES;
     }
     
     OutputLevels outputVals;
@@ -326,34 +352,56 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
         outputVals[i] = simpleData[i]/256.f;
     }
     
-    static OutputLevelsQueue levelsQueue;
-
-    OutputLevels recentAvgLevels = outputLevelsQueueAverage(levelsQueue);
-
-    TreeDisplayBits disp;
-    for (int i=0; i<kNumTreeBits; i++) {
+    
+    TreeDisplayBits treeBits;
+    {
+        static OutputLevelsQueue levelsQueue;
+        static TreeDisplayBits queuedTreeBits;
+        static TreeDisplayBits prevTreeBits;
+        static CFTimeInterval prevTime = 0;
         
-        BOOL trigger = outputVals[i] >= recentAvgLevels[i];
-        
-        if (trigger) {
-            disp.set(i);
+        if (!bPrevDidDrawArtwork && bDrawArtwork) {
+            levelsQueue.clear();
         }
         
-        levelsQueue.push_front(outputVals);
-        if (levelsQueue.size() >= kLPFSize) {
-            levelsQueue.pop_back();
+        OutputLevels recentAvgLevels = outputLevelsQueueAverage(levelsQueue);
+        for (int i=0; i<kNumTreeBits; i++) {
+            
+            BOOL trigger = outputVals[i] >= recentAvgLevels[i];
+            
+            if (trigger) {
+                treeBits.set(i);
+            }
+            
+            levelsQueue.push_front(outputVals);
+            if (levelsQueue.size() >= kLPFSize) {
+                levelsQueue.pop_back();
+            }
+        }
+        
+        CFTimeInterval currTime = CACurrentMediaTime();
+        CFTimeInterval delta = currTime - prevTime;
+        
+        if ((delta * 1000 >= 33))
+        {
+            treeBits = treeBits | queuedTreeBits;
+            prevTreeBits = treeBits;
+            prevTime = currTime;
+            queuedTreeBits.reset();
+        } else {
+            queuedTreeBits = queuedTreeBits | treeBits;
+            treeBits = prevTreeBits;
         }
     }
     
-    
-    if (1) {
+    if (0) {
         
         [[NSColor whiteColor] set];
         NSRectFill(NSMakeRect(10,500, 160, 100));
         
         for(int i=0;i<kNumTreeBits;i++){
             
-            BOOL bSet = disp[i];
+            BOOL bSet = treeBits[i];
             CGFloat height = outputVals[i];
             
             NSColor* color = [NSColor colorWithDeviceRed:(i%3==0) green:(i%3==1) blue:(i%3==2) alpha:1];
@@ -366,30 +414,148 @@ void DrawVisual( VisualPluginData * visualPluginData, ORSSerialPort* serialPort 
         }
     }
     
-    {
+    if (kEmitSimpleBits) {
+
+
+        
+        TreeDisplayBits sendBits = treeBits;
+        char bitData = static_cast<char>(sendBits.to_ulong());
+        NSData* data = [NSData dataWithBytes:&bitData length:1];
+        if (serialPort) {
+            [serialPort sendData:data];
+        }
+    }
+    
+    
+    if (kEmitLEDRibbonIntensity) {
+        
         static CFTimeInterval prevTime = 0;
-        static TreeDisplayBits prevDisp;
+
+        static std::vector<SpectrumData> heldUpData;
+        static float maxAvgIntensity = 0;
+        static SpectrumData maxSpectrumVals;
+        static uint8_t addIntensity = 0;
+        
+        if (!bPrevDidDrawArtwork && bDrawArtwork) {
+            maxAvgIntensity = 0;
+            addIntensity = 0;
+            heldUpData.clear();
+            maxSpectrumVals.clear();
+        }
         
         CFTimeInterval currTime = CACurrentMediaTime();
         CFTimeInterval delta = currTime - prevTime;
         
-        if ((delta * 1000 >= 100))
-        {
+        if ((delta * 1000) >= 60) {
+
+            // no intensity can be a full 255 since that byte value is reserved
+            SpectrumData outIntensities = ribbonData;
             
-            TreeDisplayBits sendBits = disp | prevDisp;            
-            char bitData = static_cast<char>(sendBits.to_ulong());
-            NSData* data = [NSData dataWithBytes:&bitData length:1];
+            if (heldUpData.size()) {
+
+                heldUpData.push_back(ribbonData);
+                size_t numArr = heldUpData.size();
+                
+                for (int i=0; i<ribbonData.size(); i++) {
+                    uint32_t sum = 0;
+                    for (auto v : heldUpData ) {
+                        sum += v[i];
+                    }
+                    uint8_t avg = sum / numArr;
+                    outIntensities[i] = avg;
+                }
+            }
+            
+            uint32_t totalIntensity = std::accumulate(outIntensities.begin(), outIntensities.end(), 0);
+            float avgIntensity = (float)totalIntensity / (float)outIntensities.size() / 255.f;
+            maxAvgIntensity = MAX(maxAvgIntensity, avgIntensity);
+            
+            if (avgIntensity >= 0.5 || avgIntensity >= (0.75 * maxAvgIntensity)) {
+                addIntensity = 64;
+            } else {
+                addIntensity = (float)addIntensity * 0.3;
+            }
+            
+            if (addIntensity < 10) {
+                addIntensity = 0;
+            }
+            
+            if (maxSpectrumVals.size() == outIntensities.size()) {
+                for (int i=0; i<outIntensities.size(); i++) {
+                    maxSpectrumVals[i] = MAX(outIntensities[i], maxSpectrumVals[i]);
+                }
+            } else {
+                maxSpectrumVals = SpectrumData(outIntensities);
+            }
+            
+            for (int i=0; i<outIntensities.size(); i++) {
+                
+                float maxVal = maxSpectrumVals[i];
+                maxVal = MAX(maxVal, 16);
+                
+                float val = (float)outIntensities[i] / maxVal;
+                val = MIN(1.f, MAX(0.f, val));
+                uint8_t inten = (val * 64) + addIntensity;
+                outIntensities[i] = inten == 255 ? 254 : inten;
+            }
+            
+            SpectrumData tmp = outIntensities;
+            std::reverse(tmp.begin(),tmp.end());
+            
+            SpectrumData rotatedOutput;
+            rotatedOutput.insert(rotatedOutput.end(), tmp.begin(), tmp.end());
+            tmp = outIntensities;
+            rotatedOutput.insert(rotatedOutput.end(), tmp.begin(), tmp.end());
+            
+            if (1) {
+                
+                NSBezierPath* thePath = [NSBezierPath bezierPath];
+                widthStep = viewSize.width / (CGFloat)rotatedOutput.size();
+                heightStep = viewSize.height/256;
+                NSPoint pt = NSMakePoint(widthBase,
+                                         heightBase + (rotatedOutput[0] * heightStep));
+                [thePath moveToPoint:pt];
+                for (int i=0; i<rotatedOutput.size(); i++) {
+                    pt = NSMakePoint(widthBase + i*widthStep,
+                                     heightBase + (rotatedOutput[i] * heightStep));
+                    [thePath lineToPoint:pt];
+                }
+                [[NSColor purpleColor] set];
+                [thePath setLineWidth:3];
+                [thePath stroke];
+                
+                /*
+                 [[NSColor whiteColor] set];
+                 NSRectFill(NSMakeRect(10,500, 160, 100));
+                 {
+                    CGFloat height = avgIntensity;
+                    NSColor* color = [NSColor purpleColor];
+                    [color set];
+                    NSRectFill(NSMakeRect(10,500, 40, 100 * height));
+                 }
+                 */
+            }
+            
+            // add the bits to control the basic lights
+            rotatedOutput.push_back((UInt8)treeBits.to_ulong());
+
+            // add a footer of 255 to say we are done with this command
+            rotatedOutput.push_back(255);
+            
+            NSData* data = [NSData dataWithBytes:rotatedOutput.data() length:rotatedOutput.size() * sizeof(uint8_t)];
             if (serialPort) {
                 [serialPort sendData:data];
             }
-
-            prevTime = currTime;
-            prevDisp.reset();
+            
+            heldUpData.clear();
+            
         } else {
-            prevDisp = prevDisp | disp;
+            heldUpData.push_back(ribbonData);
         }
+        
     }
 	
+    bPrevDidDrawArtwork = bDrawArtwork;
 
 }
 
@@ -593,8 +759,11 @@ OSStatus ConfigureVisual( VisualPluginData * visualPluginData )
             NSLog(@"Christmas Tree Visualizer: found serial port name %@", serialPortName);
             
             _serialPort = [ORSSerialPort serialPortWithPath:serialPortName];
-            _serialPort.baudRate = [NSNumber numberWithInteger:9600];
+            _serialPort.baudRate = [NSNumber numberWithInteger:115200];
             [_serialPort open];
+            
+            usleep(500000);
+            
         } else {
             NSLog(@"Christmas Tree Visualizer Error: Could not make a serial port connection");
         }
